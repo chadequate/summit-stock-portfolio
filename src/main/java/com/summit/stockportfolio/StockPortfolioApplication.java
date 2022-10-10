c2a321142f2d4b41d34ba25e015670d670aba453
Chad Lewis <chadlewis@Chads-MacBook-Air.local>
2022-10-10 16:10:38 -0400

    First commit

---
+++ src/main/java/com/summit/stockportfolio/StockPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.summit.stockportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPortfolioApplication.class, args);
+	}
+
+}
